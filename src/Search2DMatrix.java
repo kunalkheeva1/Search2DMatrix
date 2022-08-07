27d5cb98f36c08d9d925b5147d6fcaac4080f5bb
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-06 22:10:26 -0700

    coded a method in to find the target in 2d array, but the condition is array is sorted from left to right and top to bottom

--- src/Search2DMatrix.java
+++ src/Search2DMatrix.java
@@ -1,5 +1,28 @@
 public class Search2DMatrix {
+
+    public static boolean search2DMatrix(int [][] arr, int target){
+        int n = arr.length;
+        int m = arr[0].length;
+        int i=0, j=m-1;
+        while(i<n && j>=0){
+            if(arr[i][j]> target){
+                j--;
+            }else if(arr[i][j]< target){
+                i++;
+            }else if(arr[i][j]==target){
+                return true;
+            }
+
+        }return false;
+
+
+
+
+    }
+
+
+
     public static void main(String[] args) {
-        
+
     }
 }
