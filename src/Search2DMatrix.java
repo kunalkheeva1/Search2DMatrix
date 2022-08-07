ef8a66610d72264fc5d862f7a392066d237e2ede
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-06 22:18:55 -0700

    added comments for the better understanding

--- src/Search2DMatrix.java
+++ src/Search2DMatrix.java
@@ -1,6 +1,14 @@
 public class Search2DMatrix {
 
+
+    //return true if target is found else false but not in On^2, you can use ON
+    //condition is array is sorted from top to bottom, left to right
     public static boolean search2DMatrix(int [][] arr, int target){
+
+        //n is no. of rows, m is no. of columns
+        //basically we need to start from the right top most block
+        //as if the target is greater, then we can move downwards to check if there is one present
+        // or if its smaller, then we can move to left side to check the row
         int n = arr.length;
         int m = arr[0].length;
         int i=0, j=m-1;
